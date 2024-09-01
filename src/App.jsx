3ecf2d41c7f751301ef4cbafcbe30620e043aab4
Chris <iece.chris@gmail.com>
2024-09-01 11:28:18 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Home from "./home.jsx"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+          <Home/>
+          <h1> hihihi</h1>
+          <h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1>
+          <h1> hihihi</h1>
+          <h1> hihihi</h1>
+
+          <h1> hihihi</h1>
+          <h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1>
+          <h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1>
+          <h1> hihihi</h1><h1> hihihi</h1><h1> hihihi</h1>
+
+
+      </>
+  )
+}
+
+export default App
