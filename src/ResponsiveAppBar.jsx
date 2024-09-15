58a50d7d6b3277858cf8a671845818075d8c936e
Chris <iece.chris@gmail.com>
2024-09-16 00:42:38 +1000

    Fix: Clicks Not Registering After Closing Fullscreen Menu

--- src/ResponsiveAppBar.jsx
+++ src/ResponsiveAppBar.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, Button, MenuItem, Slide } from '@mui/material';
+import { AppBar, Box, Toolbar, IconButton, Typography, Button, MenuItem, Slide, useMediaQuery  } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { Link } from 'react-router-dom';
@@ -14,6 +14,9 @@ import ToggleOnIcon from '@mui/icons-material/ToggleOn';
 import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 
 const ResponsiveAppBar = () => {
+
+
+
     const [menuOpen, setMenuOpen] = useState(false);
     const theme = useTheme();
     const isDarkMode = useSelector((state) => state.theme.mode === 'dark'); // Get theme mode from Redux
@@ -29,12 +32,14 @@ const ResponsiveAppBar = () => {
         setMenuOpen(false); // Close menu
     };
 
+    const isMobile = useMediaQuery('(min-width:1000px)');
     return (
         <>
             <AppBar
                 position="fixed"
                 sx={{
-                    backgroundColor: isDarkMode ? 'rgba(0,0,0,0.6)' : 'rgba(0,0,0,0.64)',
+                    backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)',
+                    backdropFilter: 'blur(10px)',
                     transition: 'background-color 0.3s ease',
                     boxShadow: 'none',
                     zIndex: (theme) => theme.zIndex.drawer + 1,
@@ -49,15 +54,15 @@ const ResponsiveAppBar = () => {
                         sx={{
                             flexGrow: 1,
                             fontFamily: 'Roboto, sans-serif',
-                            fontWeight: 500,
-                            letterSpacing: '0.02rem',
-                            color: "white",
+                            fontWeight: 400,
+                            letterSpacing: '0rem',
+                            color: theme.palette.text.primary,
                             textDecoration: 'none',
                             textAlign: 'left',
                             fontSize: {
                                 xs: '1.2rem',
-                                sm: '1.4rem',
-                                md: '1.6rem',
+                                sm: '1.3rem',
+                                md: '1.4rem',
                             },
                         }}
                     >
@@ -65,44 +70,53 @@ const ResponsiveAppBar = () => {
                     </Typography>
 
                     {/* Display navigation items on larger screens */}
-                    <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
-                        {['Home', 'Shopping', 'Services', 'Booking', 'Contact', 'FAQ'].map((page) => (
-                            <Button
-                                key={page}
-                                component={Link}
-                                to={page.toLowerCase() === 'home' ? '/' : `/${page.toLowerCase()}`}
-                                onClick={handleCloseNavMenu}
-                                sx={{
-                                    my: 2,
-                                    color: 'white',
-                                    display: 'block',
-                                    fontSize: '1rem',
-                                }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
-                    </Box>
+                    {isMobile && (
+                        <Box sx={{ display: 'flex', gap: 2 }}>
+                            {['Home', 'Shopping', 'Services', 'Booking', 'Contact', 'FAQ'].map((page) => (
+                                <Button
+                                    key={page}
+                                    component={Link}
+                                    to={page.toLowerCase() === 'home' ? '/' : `/${page.toLowerCase()}`}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{
+                                        my: 2,
+                                        color: theme.palette.text.primary,
+                                        display: 'block',
+                                        fontSize: '1rem',
+                                    }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                    )}
 
                     {/* Menu button (visible on smaller screens) */}
-                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton size="large" aria-label="menu" onClick={handleOpenNavMenu} sx={{ color: 'white' }}>
-                            <MenuIcon />
-                        </IconButton>
-                    </Box>
+                    {!isMobile && (
+                        <Box sx={{ display: 'flex'}}>
+                            <IconButton size="large" aria-label="menu" onClick={handleOpenNavMenu} sx={{ color: isDarkMode ? 'white' : 'black' }}>
+                                <MenuIcon />
+                            </IconButton>
+                        </Box>
+                    )}
+
 
                     {/* Theme toggle button */}
                     <IconButton
-                        sx={{ ml: 1, color: 'white' }}
-                        onClick={() => dispatch(toggleTheme())} // Trigger toggleTheme in Redux
+                        sx={{ ml: 1, color: isDarkMode ? 'white' : 'black' }}
+                        onClick={() => dispatch(toggleTheme())}
                     >
                         {isDarkMode ? <Brightness4Icon /> : <Brightness7Icon />}
                     </IconButton>
                     <IconButton
-                        sx={{ ml: 1, color: 'white' }}
-                        onClick={() => dispatch(toggleTheme())} // Trigger toggleTheme in Redux
+                        sx={{ ml: 1, color: isDarkMode ? 'white' : 'black' }}
+                        onClick={() => dispatch(toggleTheme())}
                     >
-                        {isDarkMode ? <ToggleOnIcon sx={{fontSize: "45px"}} /> : <ToggleOffIcon sx={{fontSize: "45px"}} />}
+                        {isDarkMode ? (
+                            <ToggleOnIcon sx={{ fontSize: '45px' }} />
+                        ) : (
+                            <ToggleOffIcon sx={{ fontSize: '45px' }} />
+                        )}
                     </IconButton>
 
                     {/* ShoppingCart */}
@@ -119,7 +133,8 @@ const ResponsiveAppBar = () => {
                         left: 0,
                         width: '100vw',
                         height: '100vh',
-                        backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.9)' : 'white',
+                        backdropFilter: 'blur(10px)',
+                        backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)',
                         color: isDarkMode ? 'white' : 'black',
                         display: 'flex',
                         flexDirection: 'column',
